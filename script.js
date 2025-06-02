34821332002b4be6fc724c9178de25530e32ab12
Huseynliİlqar <huseynilqarr@gmail.com>
2025-06-02 17:48:52 +0400

    navbar add burger

--- script.js
+++ script.js
@@ -1,18 +1,6 @@
 function mobileMenuToggle() {
     const menu = document.getElementById('mobile_navbar');
-    if (menu.style.display === 'flex') {
-        menu.style.display = 'none';
-    } else {
-        menu.style.display = 'flex';
-    }
+        menu.classList.toggle('open');
 }
 
-function mobileMenuToggle() {
-    const menu = document.getElementById('mobile_navbar');
-    if (menu.style.display === 'none') {
-        menu.style.display = 'flex';
-    } else {
-        menu.style.display = 'none';
-    }
-}
 
